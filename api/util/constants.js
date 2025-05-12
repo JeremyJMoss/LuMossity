a3d96c18f8d97346a11a3c57b5b01e5d50a97da2
Jeremy J Moss <mossyj19@gmail.com>
2025-05-12 22:12:53 +1000

    updated field config

--- api/util/constants.js
+++ api/util/constants.js
@@ -3,6 +3,31 @@ const path = require('path');
 module.exports.configPath = path.resolve(__dirname, '..', 'config', 'db.json');
 module.exports.presetsPath = path.join(__dirname, '..', 'seeds', 'field_presets.json');
 module.exports.maxFailedLoginAttempts = 5;
-// how long till user can login again
 module.exports.lockoutBaseTime = 15 * 60;
 module.exports.refreshTokenMaxAge = 7 * 24 * 60 * 1000;
+
+module.exports.fieldTypeToMySQLType = {
+  text: 'VARCHAR(255)',
+  textarea: 'TEXT',
+  email: 'VARCHAR(255)',
+  integer: 'INT',
+  float: 'FLOAT',
+  currency: 'DECIMAL(10, 2)',
+  url: 'VARCHAR(2083)',
+  tel: 'VARCHAR(20)',
+  password: 'VARCHAR(255)',
+  date: 'DATE',
+  time: 'TIME',
+  datetime: 'DATETIME',
+  checkboxes: 'JSON',
+  checkbox: 'BOOLEAN',
+  radio: 'VARCHAR(255)',
+  switch: 'BOOLEAN',
+  select: 'VARCHAR(255)',
+  multiselect: 'JSON',
+  image: 'JSON',
+  file: 'JSON',
+  relation_select: 'INT',
+  relation_multiselect: "JSON",
+  group: 'JSON'
+};
