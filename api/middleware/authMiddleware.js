1de106ce6d3916ec7879c6e88e77c034a994f551
Jeremy J Moss <mossyj19@gmail.com>
2025-05-24 20:01:21 +1000

    added authorization header back in

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -10,13 +10,14 @@ const logger = require('../models/utility/Logger');
 const authenticate = (requiredRole = null) => {
     return async (req, res, next) => {
         try {
-            const access_token = req.cookies?.accessToken;
-            if (!access_token) {
-                logger.debug(`Request sent from ${req.ip } has missing or invalid access token.`);
-                throw new AuthenticationError('Missing or invalid access token');
+            const authHeader = req.headers['authorization'];
+            if (!authHeader || !authHeader.startsWith('Bearer ')) {
+                logger.debug(`Request sent from ${req.ip } has missing or invalid Authorization header.`);
+                throw new AuthenticationError('Missing or invalid Authorization header');
             }
 
-            const decoded = verifyJwtToken('access', access_token);
+            const token = authHeader.split(' ')[1];
+            const decoded = verifyJwtToken('access', token);
 
             if (!decoded || !decoded.userId) {
                 logger.debug(`Request sent from ${req.ip} with invalid or expired token.`);
